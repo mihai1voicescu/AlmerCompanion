d0483a0ebebcce2454cd086372ac0ae6310bc931
Mihai Voicescu <mihai@almer-technologies.com>
2021-12-08 14:54:11 +0100

    Make 2 versions of InfoItem

--- app/src/main/java/io/almer/almercompanion/screen/main/Info.kt
+++ app/src/main/java/io/almer/almercompanion/screen/main/Info.kt
@@ -1,16 +1,15 @@
 package io.almer.almercompanion.screen.main
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -36,6 +35,24 @@ object Info : MainScreenType(R.drawable.ic_round_info_24, R.string.navigation_it
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun InfoItem(
+    name: String,
+    icon: Painter,
+    value: String,
+    onClick: () -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .padding(10.dp, 2.dp),
+        shape = MaterialTheme.shapes.large,
+        onClick = onClick
+    ) {
+        InfoItemContent(name, icon, value)
+    }
+}
+
 @Composable
 fun InfoItem(
     name: String,
@@ -45,34 +62,43 @@ fun InfoItem(
     Surface(
         modifier = Modifier
             .padding(10.dp, 2.dp),
-        shape = MaterialTheme.shapes.large
+        shape = MaterialTheme.shapes.large,
     ) {
-        Row(
-            Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                painter = icon,
-                contentDescription = name,
-                modifier = Modifier
-                    .size(40.dp)
-            )
-            Text(
-                text = name,
-                style = MaterialTheme.typography.button,
-                modifier = Modifier
-                    .padding(16.dp)
-                    .weight(1f)
-            )
+        InfoItemContent(name, icon, value)
+    }
+}
 
-            Text(
-                text = value,
-                textAlign = TextAlign.Right,
-                style = MaterialTheme.typography.button,
-                modifier = Modifier
-                    .padding(16.dp)
-            )
-        }
+@Composable
+private fun InfoItemContent(
+    name: String,
+    icon: Painter,
+    value: String,
+) {
+    Row(
+        Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = icon,
+            contentDescription = name,
+            modifier = Modifier
+                .size(40.dp)
+        )
+        Text(
+            text = name,
+            style = MaterialTheme.typography.button,
+            modifier = Modifier
+                .padding(16.dp)
+                .weight(1f)
+        )
+
+        Text(
+            text = value,
+            textAlign = TextAlign.Right,
+            style = MaterialTheme.typography.button,
+            modifier = Modifier
+                .padding(16.dp)
+        )
     }
 }
 
